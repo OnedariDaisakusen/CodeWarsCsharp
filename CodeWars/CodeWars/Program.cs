05ac4d9f0baf89d5f7822013be02cc053aaf6973
OnedariDaisakusen <novagit666@gmail.com>
2020-05-20 16:00:54 -0500

    Primer Commit

---
+++ CodeWars/CodeWars/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Cantidad de unos  : " + CountBits(1234));
+
+            Console.ReadLine();
+        }
+
+        public static int CountBits(int n)
+        {
+            Console.WriteLine("Numero : " + n);
+            string binario = Convert.ToString(n, 2);
+            Console.WriteLine("Binario : " + binario);
+            int cont = 0;
+            for(int  i = 0; i< binario.Length; i++)
+            {
+                if (binario[i].Equals('1'))
+                {
+                    cont++;
+                }
+
+
+            }
+
+            return cont;
+        }
+    }
+}
